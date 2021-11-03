6b4273b5285428b2f1416c8980b7f7422d7a6aed
liuziyee <liuziyee1994@163.com>
2021-11-03 17:48:37 +0800

    :wrench:

--- src/main/java/com/dorohedoro/wiki/bootstrap/WikiApplication.java
+++ src/main/java/com/dorohedoro/wiki/bootstrap/WikiApplication.java
@@ -1,10 +1,12 @@
 package com.dorohedoro.wiki.bootstrap;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.dorohedoro")
+@MapperScan("com.dorohedoro.wiki.mapper")
 @SpringBootApplication
 public class WikiApplication {
 
