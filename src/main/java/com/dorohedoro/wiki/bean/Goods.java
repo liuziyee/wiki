9b70b1bfb1600b857622767ffbb411e2a81ae0a5
liuziyee <liuziyee1994@163.com>
2021-11-04 09:35:30 +0800

    :card_file_box:

---
+++ src/main/java/com/dorohedoro/wiki/bean/Goods.java
@@ -0,0 +1,103 @@
+package com.dorohedoro.wiki.bean;
+
+public class Goods {
+    private Long id;
+
+    private String name;
+
+    private Long pid;
+
+    private String desc;
+
+    private String cover;
+
+    private Long viewCount;
+
+    private Long followCount;
+
+    private Boolean status;
+
+    private Long createTime;
+
+    private Long updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc == null ? null : desc.trim();
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover == null ? null : cover.trim();
+    }
+
+    public Long getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Long viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Long getFollowCount() {
+        return followCount;
+    }
+
+    public void setFollowCount(Long followCount) {
+        this.followCount = followCount;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/dorohedoro/wiki/mapper/GoodsMapper.java
@@ -0,0 +1,17 @@
+package com.dorohedoro.wiki.mapper;
+
+import com.dorohedoro.wiki.bean.Goods;
+
+public interface GoodsMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Goods record);
+
+    int insertSelective(Goods record);
+
+    Goods selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Goods record);
+
+    int updateByPrimaryKey(Goods record);
+}
