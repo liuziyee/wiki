ecb0aee5ceb171d43d19afbb09d2d0738886e2ed
liuziyee <liuziyee1994@163.com>
2021-11-05 09:45:38 +0800

    :zap:

--- src/main/java/com/dorohedoro/wiki/service/GoodsService.java
+++ src/main/java/com/dorohedoro/wiki/service/GoodsService.java
@@ -5,6 +5,7 @@ import com.dorohedoro.wiki.bean.GoodsExample;
 import com.dorohedoro.wiki.bean.ResponseBean;
 import com.dorohedoro.wiki.mapper.GoodsMapper;
 import com.dorohedoro.wiki.util.AppEnum;
+import com.dorohedoro.wiki.util.BeanUtil;
 import com.dorohedoro.wiki.util.TimeUtil;
 import com.dorohedoro.wiki.vo.GoodsVO;
 import org.springframework.beans.BeanUtils;
@@ -33,13 +34,7 @@ public class GoodsService {
         criteria.andNameLike("%" + goods.getName() + "%");
         List<Goods> goodsList = goodsMapper.selectByExample(goodsExample);
 
-        ArrayList<GoodsVO> goodsVOList = new ArrayList<>();
-        for (Goods bean : goodsList) {
-            GoodsVO goodsVO = new GoodsVO();
-            BeanUtils.copyProperties(bean, goodsVO);
-            goodsVOList.add(goodsVO);
-        }
-        
+        List<GoodsVO> goodsVOList = BeanUtil.copyList(goodsList, GoodsVO.class);
         return goodsVOList;
     }
 
---
+++ src/main/java/com/dorohedoro/wiki/util/BeanUtil.java
@@ -0,0 +1,40 @@
+package com.dorohedoro.wiki.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author liuziye
+ * @Date 2021/11/5 9:06
+ */
+public class BeanUtil {
+    public static <T> T copy(Object source, Class<T> clazz) {
+        if (source == null) {
+            return null;
+        }
+        T obj = null;
+        try {
+            obj = clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        BeanUtils.copyProperties(source, obj);
+        return obj;
+    }
+
+    public static <T> List<T> copyList(List source, Class<T> clazz) {
+        List<T> target = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(source)) {
+            for (Object o : source) {
+                T obj = copy(o, clazz);
+                target.add(obj);
+            }
+        }
+        return target;
+    }
+}
