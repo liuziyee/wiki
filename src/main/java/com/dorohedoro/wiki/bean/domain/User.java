c69af3f4a5cffc274e449bd1585ee493bc47bab6
liuziyee <liuziyee1994@163.com>
2021-11-24 11:09:14 +0800

    :beers:

---
+++ src/main/java/com/dorohedoro/wiki/bean/domain/User.java
@@ -0,0 +1,48 @@
+package com.dorohedoro.wiki.bean.domain;
+
+import com.dorohedoro.wiki.bean.VO.PageBean;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+public class User extends PageBean {
+    private Long id;
+    
+    private String loginName;
+    
+    private String name;
+    
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName == null ? null : loginName.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
