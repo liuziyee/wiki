c37a2ee0464f0a2815bb52a466fb94d76192c305
liuziyee <liuziyee1994@163.com>
2023-01-25 16:25:40 +0800

    :beers:

--- src/main/java/com/dorohedoro/wiki/job/DemoJob.java
+++ src/main/java/com/dorohedoro/wiki/job/DemoJob.java
@@ -2,24 +2,21 @@ package com.dorohedoro.wiki.job;
 
 import com.dorohedoro.wiki.util.TimeUtil;
 import com.dorohedoro.wiki.websocket.WSServer;
-import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.time.Instant;
 
-@Slf4j
-public class TimestampJob extends QuartzJobBean {
+public class DemoJob extends QuartzJobBean {
     
-    public static final String ID = "timestamp";
+    public static final String ID = "demo";
 
     @Autowired
     private WSServer wsServer;
     
     @Override
-    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext context) {
         wsServer.broadcastMsg(TimeUtil.getYMDHMS(Instant.now().toEpochMilli()), "");
     }
 }
